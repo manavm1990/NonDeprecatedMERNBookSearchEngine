fb3b9308d0b89994dff0a77e17c093008b0745ca
Manav Misra <manavm@visionwebsoft.com>
2023-03-22 09:02:06 -0500

    Customize error message for duplicate username

--- server/app/user/controller.js
+++ server/app/user/controller.js
@@ -2,9 +2,15 @@ import User from "./index.js";
 
 const controller = {
   async create(newUser) {
-    const createdUser = await User.create(newUser);
+    try {
+      const createdUser = await User.create(newUser);
 
-    return createdUser.authenticate(newUser.password);
+      return createdUser.authenticate(newUser.password);
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new Error("Username already exists");
+      }
+    }
   },
   async show(username, password) {
     const user = await User.findOne({ username });
