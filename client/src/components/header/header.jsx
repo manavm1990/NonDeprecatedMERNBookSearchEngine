5aa24169365368dfc50b306ef2cfbf9b19855300
Manav Misra <manavm@visionwebsoft.com>
2023-03-22 00:36:31 -0500

    refactor: :building_construction: simplify

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,13 +1,49 @@
+import AuthContext from "@/context";
 import PropTypes from "prop-types";
-import Navigation from "./navigation/navigation";
+import { useContext, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/esm/Stack";
+import RegisterLogin from "./register-login/register-login";
 import SearchBar from "./search-bar";
 
 export default function Header({ handleSearch }) {
+  const [isShowingRegisterLogin, setIsShowingRegisterLogin] = useState(false);
+  const currentUser = useContext(AuthContext);
+
   return (
-    <header className="bg-dark pt-5 pb-3">
-      <Navigation />
+    <Container fluid as="header" className="bg-dark pt-5 pb-3 px-5">
+      <Stack direction="horizontal">
+        <h1 className="text-white">Search For 📚</h1>
+        {currentUser ? (
+          <Button
+            variant="danger"
+            className="ms-auto"
+            onClick={() => {
+              localStorage.removeItem("token");
+              window.location.assign("/");
+            }}
+          >
+            Logout
+          </Button>
+        ) : (
+          <Button
+            onClick={() => setIsShowingRegisterLogin(true)}
+            className="ms-auto"
+          >
+            Login/Register
+          </Button>
+        )}
+      </Stack>
       <SearchBar handleSubmit={handleSearch} />
-    </header>
+
+      <RegisterLogin
+        isShowing={isShowingRegisterLogin}
+        hide={() => {
+          setIsShowingRegisterLogin(false);
+        }}
+      />
+    </Container>
   );
 }
 
