d0ddec1d54aef9667f99ceec212f3fb802a9470a
Manav Misra <manavm@visionwebsoft.com>
2023-03-22 00:58:54 -0500

    Add toggle switch

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { useContext, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Stack from "react-bootstrap/esm/Stack";
 import RegisterLogin from "./register-login/register-login";
 import SearchBar from "./search-bar";
@@ -12,30 +13,41 @@ export default function Header({ handleSearch }) {
   const currentUser = useContext(AuthContext);
 
   return (
-    <Container fluid as="header" className="bg-dark pt-5 pb-3 px-5">
-      <Stack direction="horizontal">
-        <h1 className="text-white">Search For 📚</h1>
-        {currentUser ? (
-          <Button
-            variant="danger"
-            className="ms-auto"
-            onClick={() => {
-              localStorage.removeItem("token");
-              window.location.assign("/");
-            }}
-          >
-            Logout
-          </Button>
-        ) : (
-          <Button
-            onClick={() => setIsShowingRegisterLogin(true)}
-            className="ms-auto"
-          >
-            Login/Register
-          </Button>
-        )}
-      </Stack>
-      <SearchBar handleSubmit={handleSearch} />
+    <>
+      <Container fluid as="header" className="bg-dark pt-5 pb-3 px-5">
+        <Stack direction="horizontal">
+          <h1 className="text-white">Search For 📚</h1>
+          {currentUser ? (
+            <Button
+              variant="danger"
+              className="ms-auto"
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.assign("/");
+              }}
+            >
+              Logout
+            </Button>
+          ) : (
+            <Button
+              onClick={() => setIsShowingRegisterLogin(true)}
+              className="ms-auto"
+            >
+              Login/Register
+            </Button>
+          )}
+        </Stack>
+        <SearchBar handleSubmit={handleSearch} />
+
+        <Form className="d-flex justify-content-center mt-4 mb-2 border-top pt-3">
+          <Form.Check
+            type="switch"
+            id="nav-switch"
+            label="Only show my saved books"
+            className="text-white"
+          />
+        </Form>
+      </Container>
 
       <RegisterLogin
         isShowing={isShowingRegisterLogin}
@@ -43,7 +55,7 @@ export default function Header({ handleSearch }) {
           setIsShowingRegisterLogin(false);
         }}
       />
-    </Container>
+    </>
   );
 }
 
