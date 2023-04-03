2f3130fd58c5ebabc334050687247d56c700fe28
Manav Misra <manavm@visionwebsoft.com>
2023-04-03 15:22:07 -0500

    Set up for Atlas for 'production'

--- server/app/config.js
+++ server/app/config.js
@@ -7,7 +7,10 @@ export default {
     expiresIn: process.env.JWT_EXPIRES_IN || "1h",
     secret: process.env.JWT_SECRET,
   },
-  mongoURL: process.env.MONGO_URL || "mongodb://localhost:27017/books",
+  mongoURL:
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGO_URL
+      : "mongodb://localhost:27017/books",
   port: process.env.PORT || 4000,
   saltRounds: process.env.SALT_ROUNDS || 10,
 };
