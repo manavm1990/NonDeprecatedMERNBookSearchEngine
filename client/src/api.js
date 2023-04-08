add0fe96b6fdf75d58664dbf0234af55c52dee2b
Manav Misra <manavm@visionwebsoft.com>
2023-04-08 18:25:48 -0500

    Update API to accommodate Stripe payment intent

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,28 @@
-const BASE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+const BASE_GOOGLE_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+const BASE_PAYMENT_URL = "http://localhost:4000/payment";
 
-export const index = async (q) => {
-  const response = await fetch(BASE_URL + q);
+// TODO: Use 'ky' to clean this 'fetch' 💩 up.
+export const indexGoogleBooks = async (q) => {
+  const response = await fetch(BASE_GOOGLE_URL + q);
+
+  if (!response.ok) {
+    const message = `An error has occurred: ${response.status}`;
+
+    // Catch and put in Error component
+    throw new Error(message);
+  }
+
+  return response.json();
+};
+
+export const createPaymentIntent = async (cart) => {
+  const response = await fetch(BASE_PAYMENT_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(cart),
+  });
 
   if (!response.ok) {
     const message = `An error has occurred: ${response.status}`;
