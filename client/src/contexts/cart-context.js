c185b699eb2e9cc1ec2d256eed3873b7c1939dbd
Manav Misra <manavm@visionwebsoft.com>
2023-04-08 14:36:36 -0500

    Allow reducer to manage quantities

--- client/src/contexts/cart-context.js
+++ client/src/contexts/cart-context.js
@@ -2,15 +2,36 @@ import { createContext } from "react";
 
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART":
+    // This covers increasing the quantity of an item in the cart
+    case "ADD_TO_CART": {
+      const { cart } = state;
+
+      // Does this item already exist in the cart?
+      const existingCartItem = cart.find(
+        (item) => item.id === action.payload.id
+      );
+
       return {
         ...state,
-        cart: [...state.cart, action.payload],
+        cart: [
+          ...cart,
+          {
+            ...action.payload,
+            quantity: existingCartItem ? existingCartItem.quantity + 1 : 1,
+          },
+        ],
       };
+    }
+
+    // This covers decreasing the quantity of an item in the cart
     case "REMOVE_FROM_CART":
       return {
         ...state,
-        cart: state.cart.filter((item) => item.id !== action.payload),
+        cart: state.cart.map((item) =>
+          item.id === action.payload.id
+            ? { ...item, quantity: item.quantity - 1 || 0 }
+            : item
+        ),
       };
   }
 };
